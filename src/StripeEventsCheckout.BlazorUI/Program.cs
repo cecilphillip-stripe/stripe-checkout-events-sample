2c60011ed754f442de16ab3824dc2ad66ac73f9e
Cecil Phillip <cecilphillip@stripe.com>
2022-08-30 23:27:40 -0400

    add toasts

--- src/StripeEventsCheckout.BlazorUI/Program.cs
+++ src/StripeEventsCheckout.BlazorUI/Program.cs
@@ -1,14 +1,16 @@
 using Blazored.SessionStorage;
+using Blazored.Toast;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using StripeEventsCheckout.BlazorUI;
-using StripeEventsCheckout.BlazorUI.Models;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddBlazoredSessionStorage();
+builder.Services.AddBlazoredToast();
+
 builder.Services.AddHttpClient("Base", client =>
     client.BaseAddress = new Uri(builder.HostEnvironment.BaseAddress));
 
