41c4f63bd1d437662ebefc5c079b5bc56d005a64
cecilphillip <cecilphillip@gmail.com>
2022-09-08 14:40:28 -0400

    include customer seeding to Mongo

--- demo_data/populate.sh
+++ demo_data/populate.sh
@@ -1,2 +1,5 @@
 echo "Importing product data...."
 mongoimport --db=eventsshop --collection=listings --file=/data/listings.json --jsonArray --mode=upsert --username=admin --password=admin --authenticationDatabase=admin
+
+echo "Importing customer...."
+mongoimport --db=eventsshop --collection=customers --file=/data/customers.json --jsonArray --mode=upsert --username=admin --password=admin --authenticationDatabase=admin
